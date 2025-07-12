acb03236d4906ca632ef0e3e39efe3cb1488dd6b
New7ech.code <New7ech.code@gmail.com>
2025-07-12 14:59:55 +0100

    good

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Categorie extends Model
 {
@@ -18,42 +17,10 @@ class Categorie extends Model
     protected $primaryKey = 'id';
     protected $fillable = [
         'name',
-        'slug', // Ajout du slug aux fillable
         'description',
         'created_by',
         'updated_by',
     ];
-
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::creating(function ($categorie) {
-            if (empty($categorie->slug)) {
-                $categorie->slug = Str::slug($categorie->name, '-');
-            }
-        });
-
-        static::updating(function ($categorie) {
-            if ($categorie->isDirty('name') && empty($categorie->slug)) {
-                 $categorie->slug = Str::slug($categorie->name, '-');
-            }
-        });
-    }
-
-    /**
-     * Get the route key for the model.
-     *
-     * @return string
-     */
-    public function getRouteKeyName()
-    {
-        return 'slug';
-    }
-
     public function articles()
     {
         return $this->hasMany(\App\Models\Article::class, 'category_id');
